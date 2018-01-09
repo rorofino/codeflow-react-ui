3a210372de9cc7aa1a3a853d289817d569dd33a4
Rodrigo Orofino <rodrigo.orofino@gmail.com>
2018-01-09 00:32:18 -0200

    making the master detail a little smarter

--- src/components/ListMasterDetail/ListMasterDetail.js
+++ src/components/ListMasterDetail/ListMasterDetail.js
@@ -4,65 +4,47 @@ import cc from 'classcat';
 import Button from "../Button/Button";
 import Modal from "../Modal/Modal";
 
-//field.split('.').reduce((a, b) => a[b], obj)
+const getDescendantProp = (obj, field) => field.split('.').reduce((a, b) => a[b], obj);
 
-const getDescendantProp = (obj, desc) => {
-    var arr = desc.split(".");
-    while(arr.length && (obj = obj[arr.shift()]));
-    return obj;
-}
+// const getDescendantProp = (obj, desc) => {
+//     var arr = desc.split(".");
+//     while(arr.length && (obj = obj[arr.shift()]));
+//     return obj;
+// }
 
 class ListMasterDetail extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            dataSources: [],
             selectedItems: []
         };
     }
 
-    handleAddClick(column, columnIndex) {
-        const {dataSources} = this.state;
-        dataSources[columnIndex] = this.state.dataSources[columnIndex].concat({
-            [column.labelProperty]: (
-                column.options ?
-                    <select>
-                        {column.options.map(option => <option value={option.value}>{option.label}</option>)}
-                    </select>
-                : <input type="text" />
-            )
-        });
-
-        this.setState({dataSources});
+    handleAddClick(column, columnIndex, selectedItems) {
+        if (this.props.onClick) {
+            this.props.onClick(column, columnIndex, selectedItems);
+        }
     }
 
     selectItem(item, column, columnIndex, itemIndex) {
-        const dataSources = [];
-        const selectedItems = [];
-        for(let i=0; i<this.props.columns.length; i++) {
-            if (i < columnIndex) {
-                dataSources[i] = this.state.dataSources[i];
-                selectedItems[i] = this.state.selectedItems[i];
-            } else if (i === columnIndex) {
-                dataSources[i] = this.state.dataSources[i];
-                selectedItems[i] = itemIndex;
-            } else if (i === columnIndex+1) {
-                dataSources[i] = item[column.valueProperty];
-                selectedItems[i] = null;
-            } else {
-                dataSources[i] = null;
-                selectedItems[i] = null;
-            }
-        }
-        this.setState({dataSources, selectedItems});
+        const selectedItems = this.state.selectedItems.slice(0, columnIndex);
+        selectedItems[columnIndex] = {value: item, index: itemIndex};
+        this.setState({selectedItems});
     }
 
     render() {
-        const columnLists = [];
         const {columns} = this.props || [];
-        const {dataSources, selectedItems} = this.state;
-        dataSources[0] = this.props.value;
+        const {selectedItems} = this.state;
+        const dataSources = [this.props.value];
+        for(let i=0; i<selectedItems.length; i++) {
+            const selectedItem = selectedItems[i];
+            const valueProperty = columns[i].valueProperty;
+            const item = dataSources[i][selectedItem.index];
+            const array = item[valueProperty];
+            dataSources.push(array);
+        }
+
         return (
             <div className="codeflow-list-master-detail">
                 <div className="codeflow-list-master-detail__title">{this.props.title}</div>
@@ -75,7 +57,7 @@ class ListMasterDetail extends React.Component {
                                 {column.manageable ? 
                                     <Button primary flat 
                                         className="codeflow-list-master-detail__column-add" 
-                                        onClick={() => this.handleAddClick(column, columnIndex)}
+                                        onClick={() => this.handleAddClick(column, columnIndex, this.state.selectedItems)}
                                         disabled={columnIndex > 0 && (selectedItems[columnIndex-1] === null || selectedItems[columnIndex-1] === undefined)}
                                         >
                                         <i className="fa fa-plus"></i>
@@ -83,13 +65,13 @@ class ListMasterDetail extends React.Component {
                                     </Button>
                                 : null }
                                     {dataSources[columnIndex] ? dataSources[columnIndex].map((item, indexItem) => { 
-                                        const itemSelected = this.state.selectedItems[columnIndex] === indexItem;
+                                        const itemSelected = this.state.selectedItems[columnIndex] && this.state.selectedItems[columnIndex].index === indexItem;
                                         return (
                                             <div key={`col${columnIndex}item${indexItem}`} 
                                                 className={cc(["codeflow-list-master-detail__column-item", {"codeflow-list-master-detail__column-item--selected": itemSelected}])}
                                                 onClick={() => this.selectItem(item, column, columnIndex, indexItem)}>
                                                 <div className="codeflow-list-master-detail__column-text">{getDescendantProp(item, column.labelProperty)}</div>
-                                                <div className={cc(["codeflow-list-master-detail__column-icon", {"codeflow-list-master-detail__column-icon--visible": itemSelected && !column.final}])}><i className="fa fa-chevron-right">></i></div>
+                                                <div className={cc(["codeflow-list-master-detail__column-icon", {"codeflow-list-master-detail__column-icon--visible": itemSelected && !column.final}])}><i className="fa fa-chevron-right"></i></div>
                                             </div>
                                         )
                                     }) : null}
@@ -107,13 +89,13 @@ class ListMasterDetail extends React.Component {
 ListMasterDetail.propTypes = {
     title: PropTypes.string.isRequired,
     titleIcon: PropTypes.string,
+    onClick: PropTypes.func,
     columns: PropTypes.arrayOf(
         PropTypes.shape({
             title:  PropTypes.string.isRequired,
             titleIcon: PropTypes.string,
             labelProperty: PropTypes.string.isRequired,
             valueProperty: PropTypes.string.isRequired,
-            groupByValue: PropTypes.bool,
             manageable: PropTypes.bool,
             final: PropTypes.bool
         })
